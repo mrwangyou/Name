d38eebf9189d92e8662cabd5c6d3df1908abfb82
wangningnan <mrwangyou@qq.com>
2020-07-10 10:10:20 +0800

    Final commit

--- wnn.py
+++ wnn.py
@@ -1,28 +1,25 @@
 import os
 
-# path = r"C:\Users\mrwan\Desktop\final_data"  # 文件夹目录
-path = r"C:\Users\mrwan\Desktop\test_data"  # 测试用
+path = r"C:\Users\mrwan\Desktop\final_data"  # 文件夹目录
+# path = r"C:\Users\mrwan\Desktop\test_data"  # 测试用
 files = os.listdir(path)  # 得到文件夹下的所有文件名称
 for file in files:  # 遍历文件夹
+    print('file')
     dataFinal = ''
     position = path + '\\' + file  # 构造绝对路径，"\\"，其中一个'\'为转义符
     with open(position, "r", encoding='utf-8') as f:  # 打开文件
         dataRaw = f.read()  # 读取文件
         wnn = 1
         while wnn:
-            print(wnn)
             head = dataRaw.find('微博内容：') + 5
             tail = dataRaw.find('点赞数：')
             data = dataRaw[head: tail]
-            dataRaw = dataRaw[tail + 4: -1]
+            dataRaw = dataRaw[tail + 4:]
             if dataRaw.find('微博内容：') == -1:  # 后面没有新微博的话停止这个用户的数据读取
-                print('stop')
                 wnn = 0
-                continue
             if (data[0: 2] == '//') or \
-               (data[0: 5] == '转发微博\n' and len(data) == 5) or \
-               (data[0: 7] == 'Repost\n' and len(data) == 7):  # 纯转发的删删掉
-                print('pure repost')
+               (data == '转发微博\n') or \
+               (data == 'Repost\n'):  # 纯转发的删删掉
                 continue
             if data.find(r"//<a href='/n/") != -1:  # 转发别人的内容删删掉
                 data = data[0: data.find(r"//<a href='/n/")]
@@ -33,27 +30,23 @@ for file in files:  # 遍历文件夹
             while head != -1:
                 tail = data.find(r"</a>") + 4
                 subData = data[head: tail]
-                print(subData)
                 if subData[0: 16] == r"<a href=" + r"/status/":  # 先删掉展开全文
-                    print('123')
                     data = data.replace(subData, '')
                     head = data.find(r"<a ")
                     continue
                 if subData[0: 12] == r"<a href='/n/":  # 处理at
-                    print('1234')
                     data = data.replace(subData, ' ttttt ')
                     head = data.find(r"<a ")
                     continue
                 if subData.find('#') != -1:  # 处理hashtags
-                    print('12345')
                     tepData = subData[subData.find('#') + 1:]
                     subText = tepData[0: tepData.find('#')]
                     data = data.replace(subData, ' ' + subText + ' ggggg ')
                     head = data.find(r"<a ")
                     continue
-                print('123456')
                 data = data.replace(subData, ' uuuuu ')
                 head = data.find(r"<a ")
+                continue
 
             # 处理表情
             head = data.find(r"<span ")
@@ -61,10 +54,12 @@ for file in files:  # 遍历文件夹
                 tail = data.find(r"</span>") + 7
                 subData = data[head: tail]
                 data = data.replace(subData, ' eeeee ')
+                head = data.find(r"<span ")
 
-            dataFinal = dataFinal + data + '\n'
+            # 结束啦
+            dataFinal = dataFinal + data
 
-
+    # 写回文件
     newFile = file
     newFile = newFile.strip('weibo')
     newPos = path + '\\' + newFile
